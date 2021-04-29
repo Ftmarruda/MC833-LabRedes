9784d41ff1e490071bafa56cbaba681564811e1f
Gabomfim <gabomfim99@gmail.com>
2021-04-29 18:40:46 -0300

    server-client connection

--- cliente.c
+++ cliente.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -24,24 +25,34 @@ Profile removeProfile(char *email);
 
 
 int main(){
-    //creating a TCP socket
+    
+    typedef struct sockaddr_in sockaddr_in;
+    typedef struct sockaddr sockaddr;
+    char *clientMessage = "OI, EU SOU O CLIENTE", response[256];
     int mySocket, connectionStatus;
+
+    //creating a TCP socket
     mySocket = socket(AF_INET, SOCK_STREAM, 0);
 
     //socket address
-    typedef struct sockaddr_in sockaddr_in;
-    typedef struct sockaddr sockaddr;
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(50000);
-    server_address.sin_addr.s_addr = INADDR_ANY;//server address -> substituir pelo IP do servidor
+    server_address.sin_addr.s_addr = INADDR_ANY;//server address -> substituir pelo IP do servidor quando em outro computado
 
     connectionStatus = connect(mySocket, (sockaddr *) &server_address, sizeof(server_address));
 
     if(connectionStatus == -1){
         printf("Connection failed\n\n");
+        close(mySocket);
         return 1;
     }
+
+    //receive message from server;
+    recv(mySocket, &response, sizeof(response), 0);
+    printf("The server has responded: %s\n", response);
+
+    close(mySocket);
 
     return 0;
 
--- servidor.c
+++ servidor.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -14,6 +15,35 @@
 
 int main(){
 
+    typedef struct sockaddr_in sockaddr_in;
+    typedef struct sockaddr sockaddr;
+
+    char serverMessage[256];
+    strcpy(serverMessage, "OI, EU SOU O SERVIDOR");
+    int serverSocket, clientSocket;
+
+    //create the server socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //define server address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(50000);
+    server_address.sin_addr.s_addr = INADDR_ANY;//server address -> substituir pelo IP do servidor quando em outro computador
+
+    //bind the socket to IP and port
+    bind(serverSocket, (sockaddr *) &server_address, sizeof(server_address));
+
+    listen(serverSocket, 1); //can have 1 connection waiting at max
+
+    //accept client connection
+    clientSocket = accept(serverSocket, NULL, NULL); //-> substituir valores de null por outras estruturas se quiser pegar o endereço do cliente
+    
+    //send message
+    send(clientSocket, serverMessage, sizeof(serverMessage), 0);
+
+    //close socket
+    close(serverSocket);
 
     //createProfile("felipe@example.com", "Felipe", "Tiago", "Disneyland, Orlando", "Counter Strike University", "2022");
     //createProfile("gabriel@example.com", "Gabriel", "Silveira", "Unicamp, Barão Geraldo", "Counter Strike University", "2022");
